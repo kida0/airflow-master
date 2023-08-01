120c88c82cddf00ec188f34540e16ecf44f2d407
kida <dayoung.kim@vitruv.co.kr>
2023-08-01 11:02:16 +0900

    add plugins folder

---
+++ plugins/shell/select_fruit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FRUIT=$1
+if [ "$FRUIT" = "APPLE" ]; then
+        echo "Hi Apple!"
+elif [ "$FRUIT" = "ORANGE" ]; then
+        echo "Hi Orange!"
+elif [ "$FRUIT" = "GRAPE" ]; then
+        echo "Hi Grape!"
+else
+        echo "Hi Nothing!"
+fi
