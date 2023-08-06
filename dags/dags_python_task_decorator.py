d6e2db73e894fa2882517cc78b60ab83c1f082a6
kida <dayoung.kim@vitruv.co.kr>
2023-08-07 08:59:37 +0900

    op_args, op_kwargs

--- dags/dags_python_task_decorator.py
+++ dags/dags_python_task_decorator.py
@@ -1,28 +1,14 @@
 
-from __future__ import annotations
-import logging
-import shutil
-import sys
-import tempfile
-import time
-from pprint import pprint
-
 import pendulum
-
 from airflow import DAG
 from airflow.decorators import task
-from airflow.operators.python import ExternalPythonOperator, PythonVirtualenvOperator
-
-log = logging.getLogger(__name__)
-
-PATH_TO_PYTHON_BINARY = sys.executable
 
 def x():
     pass
 
 with DAG(
     dag_id="dags_python_task_decorator",
-    scedule="0 2 * * 1",
+    schedule="0 2 * * 1",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
@@ -33,3 +19,5 @@ with DAG(
     
     python_task_1 = print_context("task_decorator 실행")
         
+    python_task_1
+    
